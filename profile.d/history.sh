f93b64c898a71859ef6cfb7143fe2c9d89acbf07
ebongzzang <truelove9866@gmail.com>
2017-12-11 09:37:36 +0900

    initial commit

---
+++ profile.d/history.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+HISTTIMEFORMAT="%m_%d %H:%M "
+export HISTTIMEFORMAT
---
+++ profile.d/oracle-instant.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/lib/oracle/12.2/client64/lib/
+export TNS_ADMIN=/home/sangbong/tns
